45591e7b1d67c7748cbbb3d9407f577c9a04b31a
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-06-13 22:41:52 +0530

    Structured file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
+import {ChakraProvider} from "@chakra-ui/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <BrowserRouter>
+  <ChakraProvider>
     <App />
+    </ChakraProvider>
     </BrowserRouter>
 );
 
