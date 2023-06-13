db888778c9431a856c9934659a6beb8031603428
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-06-13 18:57:46 +0530

    Structured file

---
+++ frontend/src/Routes/Allroutes.jsx
@@ -0,0 +1,13 @@
+import { Routes,Route } from "react-router-dom"
+import Home from "../Pages/Home"
+
+export const AllRoutes=()=>{
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </div>
+    )
+}
+
