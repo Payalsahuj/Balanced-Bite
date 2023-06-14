e0a434ec881b3c1ef0055c63e12791a55cc68560
Payalsahuj <115584065+Payalsahuj@users.noreply.github.com>
2023-06-14 12:12:27 +0530

    landpage

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
-
-
+import { Box,Image } from "@chakra-ui/react"
+import image from "../Image/Food_Meal_Dish_and_Fork_5K_Wallpaper.jpg"
+import "../CSS/Home.css"
 function Home(){
-    return <div>
-        <h1>Home</h1>
-    </div>
+    return <Box className="maincontainer" >
+        <Box style={{ position: 'sticky', top: 0, zIndex: 1, border:'2px solid yellow',height:'97vh' }} ><Image src={image} height={'100%'} width={'100%'} alt=""/></Box>
+        
+        <Box  style={{ position: 'sticky', top: 0, zIndex: 2,border:'2px solid blue' ,backgroundColor:'white',display:'flex',justifyContent:'center',alignItems:'center',}}>
+            <Box position='static' style={{border:'2px solid red',width:'90%'}}>
+            
+            </Box>
+
+        </Box>
+
+    </Box>
 }
 
 export default Home
