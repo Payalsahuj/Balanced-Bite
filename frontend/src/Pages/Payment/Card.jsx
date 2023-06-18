6a8cf1c2acc851d9579120642b1c338e3b338639
vinaykumar7580 <vinayhatwar16@gmail.com>
2023-06-18 21:42:02 +0530

    card and personaldetails page design completed

--- frontend/src/Pages/Payment/Card.jsx
+++ frontend/src/Pages/Payment/Card.jsx
@@ -1,166 +1,234 @@
 import { useState } from "react";
 import {
-   Box,
-   FormControl,
-   FormLabel,
-   Input,
-   Select,
-   Button,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+  SimpleGrid,
+  Image,
+  Heading,
+  Flex,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../Components/Navbar";
-import load from './loading.gif'
+import load from "./loading.gif";
 
 const Card = () => {
-   const [x,setx] = useState(false);
-   const [cardData, setCardData] = useState({
-      nameOnCard: "",
-      cardType: "",
-      cardNumber: "",
-      expiryDate: "",
-      cvv: "",
-   });
+  const [x, setx] = useState(false);
+  const [cardData, setCardData] = useState({
+    nameOnCard: "",
+    cardType: "",
+    cardNumber: "",
+    expiryDate: "",
+    cvv: "",
+  });
 
-   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setCardData((prev) => ({
-         ...prev,
-         [name]: value,
-      }));
-   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCardData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
-   const navigate = useNavigate();
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      setx(true);
-      setTimeout(() => {
-         navigate("/payment/summary");
-      }, 4000);
-      console.log(cardData);
-   };
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setx(true);
+    setTimeout(() => {
+      navigate("/payment/summary");
+    }, 4000);
+    console.log(cardData);
+  };
 
-   const handleCancel = (e)=>{
-      navigate('/')
-         }  
+  const handleCancel = (e) => {
+    navigate("/");
+  };
 
-   return (
-      <Box
-         //  bg="blue.900"
-         // maxWidth="800px"
-         mx="auto"
-         // px={["1rem", "2rem", "4rem"]}
-         py="10%"
-         minHeight="100vh"
-         borderRadius={"10%"}
-         box-shadow={" rgba(0, 0, 0, 0.35) 0px 5px 15px"}
-      >
-      <Navbar/>
-      {x ? 
-            <Box p={"10% 40%"} m={"auto"} textAlign={"center"}>
-               <img src={load} alt="Gif is running" backgroundColor="none" />
-            </Box> :
-            <Box  maxWidth="800px"
-               mx="auto"
-               px={["1rem", "2rem", "9rem"]}
-               py={["20%", 15, 20, "0%"]}>
-      
-         <form onSubmit={handleSubmit}>
-            <FormControl>
-               <FormLabel>Name on Card</FormLabel>
-               <Input
-                  type="text"
-                  name="nameOnCard"
-                  placeholder="Name On Card"
-                        required
-                  value={cardData.nameOnCard}
-                        backgroundColor={"#c1d6f3"}
-                  onChange={handleChange}
-               />
-            </FormControl>
+  return (
+    <Box
+      //  bg="blue.900"
+      // maxWidth="800px"
+      // mx="auto"
+      // // px={["1rem", "2rem", "4rem"]}
+      // py="10%"
+      // minHeight="100vh"
+      // borderRadius={"10%"}
+      // box-shadow={" rgba(0, 0, 0, 0.35) 0px 5px 15px"}
+      backgroundColor={"#0f346c"}
+    >
+      <Navbar />
+      {x ? (
+        <Box p={"10% 40%"} m={"auto"} textAlign={"center"}>
+          <img src={load} alt="Gif is running" backgroundColor="none" />
+        </Box>
+      ) : (
+        <Box>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <SimpleGrid
+            backgroundColor={"#FFFFFF"}
+            columns={[1, 1, 1, 2]}
+            spacing={5}
+            width={["90%", "90%", "80%", "70%"]}
+            margin={"auto"}
+            boxShadow={
+              "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px"
+            }
+            borderBottomRightRadius={"50px"}
+            borderTopLeftRadius={"50px"}
+            padding={["8px", "10px", "20px", "50px"]}
+          >
+            <Box
+            //  maxWidth="800px"
+            //  mx="auto"
+            //  px={["1rem", "2rem", "9rem"]}
+            //  py={["20%", 15, 20, "0%"]}
+            >
+              <Heading
+                fontFamily={"heading"}
+                color={"#002C9B"}
+                fontSize={["xl", "xl", "2xl", "3xl"]}
+              >
+                Enter Your Card Details . . .
+              </Heading>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <FormControl>
+                  <FormLabel>Name on Card</FormLabel>
+                  <Input
+                    type="text"
+                    name="nameOnCard"
+                    placeholder="Name On Card"
+                    required
+                    value={cardData.nameOnCard}
+                    backgroundColor={"#c1d6f3"}
+                    onChange={handleChange}
+                  />
+                </FormControl>
 
-            <FormControl>
-               <FormLabel>Card Type</FormLabel>
-               <Select
-                  name="cardType"
-                  // placeholder="Name On Card"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                  value={cardData.cardType}
-                  onChange={handleChange}
-               >
-               <option value="">Select The Type of Card</option>
-                  <option value="visa">Visa</option>
-                  <option value="mastercard">MasterCard</option>
-                  <option value="verve">Verve</option>
-               </Select>
-            </FormControl>
+                <FormControl>
+                  <FormLabel>Card Type</FormLabel>
+                  <Select
+                    name="cardType"
+                    // placeholder="Name On Card"
+                    required
+                    backgroundColor={"#c1d6f3"}
+                    value={cardData.cardType}
+                    onChange={handleChange}
+                  >
+                    <option value="">Select The Type of Card</option>
+                    <option value="visa">Visa</option>
+                    <option value="mastercard">MasterCard</option>
+                    <option value="verve">Verve</option>
+                  </Select>
+                </FormControl>
 
-            <FormControl>
-               <FormLabel>Card Number</FormLabel>
-               <Input
-                  type="number"
-            placeholder="Card Number"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                  name="cardNumber"
-                  value={cardData.cardNumber}
-                  onChange={handleChange}
-               />
-            </FormControl>
+                <FormControl>
+                  <FormLabel>Card Number</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="Card Number"
+                    required
+                    backgroundColor={"#c1d6f3"}
+                    name="cardNumber"
+                    value={cardData.cardNumber}
+                    onChange={handleChange}
+                  />
+                </FormControl>
 
-            <FormControl>
-               <FormLabel>Expiry Date</FormLabel>
-               <Input
-                  type="text"
-                  placeholder="MM / YY"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                  maxLength={7}
-                  name="expiryDate"
-                  value={cardData.expiryDate}
-                  onChange={handleChange}
-               />
-            </FormControl>
+                <Flex
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                  gap={5}
+                >
+                  <FormControl>
+                    <FormLabel>Expiry Date</FormLabel>
+                    <Input
+                      type="text"
+                      placeholder="MM / YY"
+                      required
+                      backgroundColor={"#c1d6f3"}
+                      maxLength={7}
+                      name="expiryDate"
+                      value={cardData.expiryDate}
+                      onChange={handleChange}
+                    />
+                  </FormControl>
 
-            <FormControl>
-               <FormLabel>CVV</FormLabel>
-               <Input
-                  type="password"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                  name="cvv"
-                  placeholder="Enter Your CVV"
-                  value={cardData.cvv}
-                  onChange={handleChange}
-               />
-            </FormControl>
+                  <FormControl>
+                    <FormLabel>CVV</FormLabel>
+                    <Input
+                      type="password"
+                      required
+                      backgroundColor={"#c1d6f3"}
+                      name="cvv"
+                      placeholder="Enter Your CVV"
+                      value={cardData.cvv}
+                      onChange={handleChange}
+                    />
+                  </FormControl>
+                </Flex>
+                <br />
+                <Button
+                  bg={"#0f346c"}
+                  colorScheme={"white"}
+                  variant={"solid"}
+                  type="submit"
+                >
+                  Submit Card Details
+                </Button>
+                <Button
+                  onClick={handleCancel}
+                  m={5}
+                  bg={"red.500"}
+                  colorScheme={"white"}
+                  variant={"solid"}
+                >
+                  Cancel Your Meal
+                </Button>
+              </form>
 
-            <Button bg={"green"} color={"white"} type="submit">Submit Card Details</Button>
-         <Button onClick={handleCancel} m={5} bg={"red"} color={"white"}>Cancel Your Meal</Button>
-
-         </form>
-
-         <Box mt={8} display="flex" justifyContent="center">
-            <img
-               src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
-               alt="Visa"
-               style={{ width: "50px", marginRight: "10px" }}
-            />
-            <img
-               src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
-               alt="MasterCard"
-               style={{ width: "50px", marginRight: "10px" }}
-            />
-            <img
-               src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
-               alt="Verve"
-               style={{ width: "50px" }}
-            />
-         </Box>
-         </Box>
-      }
-      </Box>
-   );
+              <Box mt={8} display="flex" justifyContent="center">
+                <img
+                  src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
+                  alt="Visa"
+                  style={{ width: "50px", marginRight: "10px" }}
+                />
+                <img
+                  src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
+                  alt="MasterCard"
+                  style={{ width: "50px", marginRight: "10px" }}
+                />
+                <img
+                  src="https://www.freepnglogos.com/uploads/visa-and-mastercard-logo-26.png"
+                  alt="Verve"
+                  style={{ width: "50px" }}
+                />
+              </Box>
+            </Box>
+            <Box>
+              <br />
+              <Image
+                borderBottomRightRadius={"50px"}
+                src={
+                  "https://img.freepik.com/free-vector/smart-id-card-abstract-concept-illustration_335657-1814.jpg?size=626&ext=jpg&ga=GA1.2.1056998353.1659457435&semt=ais"
+                }
+              />
+            </Box>
+          </SimpleGrid>
+        </Box>
+      )}
+      <br />
+      <br />
+      <br />
+    </Box>
+  );
 };
 
 export default Card;
--- frontend/src/Pages/Payment/PersonalDetails.jsx
+++ frontend/src/Pages/Payment/PersonalDetails.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import {
-   Box,
-   Button,
-   FormControl,
-   FormLabel,
-   Heading,
-   Input,
-   Select,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  Select,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import Card from "./Card";
 import { useNavigate } from "react-router-dom";
@@ -14,185 +16,227 @@ import Navbar from "../../Components/Navbar";
 import load from "./loading.gif";
 
 const PersonalDetails = () => {
-   const [stage, setStage] = useState(1);
+  const [stage, setStage] = useState(1);
 
-   const [detail, setDetail] = useState({
-      name: "",
-      email: "",
-      Address: "",
-      city: "",
-      nation: "",
-      contact: "",
-      state: "",
-   });
+  const [detail, setDetail] = useState({
+    name: "",
+    email: "",
+    Address: "",
+    city: "",
+    nation: "",
+    contact: "",
+    state: "",
+  });
 
-   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setDetail((prev) => ({
-         ...prev,
-         [name]: value,
-      }));
-   };
-   const [x, setx] = useState(false);
-   const navigate = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetail((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const [x, setx] = useState(false);
+  const navigate = useNavigate();
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      setx(true);
-      setTimeout(() => {
-         navigate("/payment/card");
-      }, 3000);
-      console.log(detail);
-   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setx(true);
+    setTimeout(() => {
+      navigate("/payment/card");
+    }, 3000);
+    console.log(detail);
+  };
 
-   const handleCancel = (e) => {
-      navigate("/");
-   };
-   return (
-      <Box>
-         <Navbar />
-         {x ? (
-            <Box p={"10% 40%"} m={"auto"} textAlign={"center"}>
-               <img src={load} alt="Gif is running" backgroundColor="none" />
-            </Box>
-         ) : (
+  const handleCancel = (e) => {
+    navigate("/");
+  };
+  return (
+    <Box backgroundColor={"#0f346c"}>
+      <Navbar />
+      {x ? (
+        <Box p={"10% 40%"} m={"auto"} textAlign={"center"}>
+          <img src={load} alt="Gif is running" backgroundColor="none" />
+        </Box>
+      ) : (
+        <Box>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <SimpleGrid
+            backgroundColor={"#FFFFFF"}
+            columns={[1, 1, 1, 2]}
+            spacing={5}
+            width={["90%", "90%", "80%", "70%"]}
+            margin={"auto"}
+            boxShadow={
+              "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px"
+            }
+            borderBottomRightRadius={"50px"}
+            borderTopLeftRadius={"50px"}
+            padding={["8px", "10px", "20px", "50px"]}
+          >
             <Box
-               maxWidth="800px"
-               mx="auto"
-               px={["1rem", "2rem", "9rem"]}
-               py={["20%", 15, 20, "5%"]}
+            // maxWidth="800px"
+            // mx="auto"
+            // px={["1rem", "2rem", "9rem"]}
+            // py={["20%", 15, 20, "5%"]}
             >
-            <Heading>
-               Enter Your delivery Address
-            </Heading>
-               <form spacing={4} as="form" onSubmit={handleSubmit}>
-                  <FormControl>
-                     <FormLabel>Name</FormLabel>
-                     <Input
-                        type="text"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                        value={detail.name}
-                        name="name"
-                        placeholder="User's Name"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
+              <Heading
+                fontFamily={"heading"}
+                color={"#002C9B"}
+                fontSize={["xl", "xl", "2xl", "3xl"]}
+              >
+                Enter Your Delivery Address . . .
+              </Heading>
+              <br />
+              <form spacing={4} as="form" onSubmit={handleSubmit}>
+                <FormControl>
+                  <FormLabel>Name</FormLabel>
+                  <Input
+                    type="text"
+                    required
+                    backgroundColor={"#c1d6f3"}
+                    value={detail.name}
+                    name="name"
+                    placeholder="User's Name"
+                    onChange={handleChange}
+                  />
+                </FormControl>
 
-                  <FormControl>
-                     <FormLabel>Email Address</FormLabel>
-                     <Input
-                        type="text"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                        value={detail.email}
-                        name="email"
-                        placeholder="User's email"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
+                <FormControl>
+                  <FormLabel>Email Address</FormLabel>
+                  <Input
+                    type="text"
+                    required
+                    backgroundColor={"#c1d6f3"}
+                    value={detail.email}
+                    name="email"
+                    placeholder="User's email"
+                    onChange={handleChange}
+                  />
+                </FormControl>
 
-                  <FormControl>
-                     <FormLabel>Address</FormLabel>
-                     <Input
-                        type="text"
-                        backgroundColor={"#c1d6f3"}
-                        required
-                        value={detail.Address}
-                        name="Address"
-                        placeholder="User's permanent address"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
+                <FormControl>
+                  <FormLabel>Address</FormLabel>
+                  <Input
+                    type="text"
+                    backgroundColor={"#c1d6f3"}
+                    required
+                    value={detail.Address}
+                    name="Address"
+                    placeholder="User's permanent address"
+                    onChange={handleChange}
+                  />
+                </FormControl>
 
-                  <FormControl>
-                     <FormLabel>City</FormLabel>
-                     <Input
-                        type="text"
-                        backgroundColor={"#c1d6f3"}
-                        required
-                        value={detail.city}
-                        name="city"
-                        placeholder="User's City"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
-                  <FormControl>
-                     <FormLabel>Nation</FormLabel>
-                     <Select
-                        backgroundColor={"#c1d6f3"}
-                        placeholder="Select option"
-                        required
-                        value={detail.nation}
-                        name="nation"
-                        onChange={handleChange}
-                     >
-                        <option value="Afghanistan">Afghanistan</option>
-                        <option value="Australia">Australia</option>
-                        <option value="Canada">Canada</option>
-                        <option value="China">China</option>
-                        <option value="Germany">Germany</option>
-                        <option value="India">India</option>
-
-                        <option value="Nepal">Nepal</option>
-
-                        <option value="Sri Lanka">Sri Lanka</option>
-
-                        <option value="United Kingdom">United Kingdom</option>
-                        <option value="Ukraine">Ukraine</option>
-                        <option value="United States">USA</option>
-                     </Select>
-                       
-                  </FormControl>
-
-                  <FormControl>
-                     <FormLabel>Contact Number</FormLabel>
-                     <Input
-                        type="number"
-                        backgroundColor={"#c1d6f3"}
-                        required
-                        value={detail.contact}
-                        name="contact"
-                        placeholder="Contact Number"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
-
-                  <FormControl>
-                     <FormLabel>State</FormLabel>
-                     <Input
-                        type="text"
-                        required
-                        backgroundColor={"#c1d6f3"}
-                        value={detail.state}
-                        name="state"
-                        placeholder="State"
-                        onChange={handleChange}
-                     />
-                  </FormControl>
-
-                  <Button
-                     bg={"green"}
-                     color={"white"}
-                     type="submit"
-                     value={"submit"}
+                <FormControl>
+                  <FormLabel>City</FormLabel>
+                  <Input
+                    type="text"
+                    backgroundColor={"#c1d6f3"}
+                    required
+                    value={detail.city}
+                    name="city"
+                    placeholder="User's City"
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Nation</FormLabel>
+                  <Select
+                    backgroundColor={"#c1d6f3"}
+                    placeholder="Select option"
+                    required
+                    value={detail.nation}
+                    name="nation"
+                    onChange={handleChange}
                   >
-                     Submit Personal Info
-                  </Button>
-                  <Button
-                     onClick={handleCancel}
-                     m={5}
-                     bg={"red"}
-                     color={"white"}
-                  >
-                     Cancel Your Meal
-                  </Button>
-               </form>
-               {/* <Input type="submit" value="Submit" /> */}
+                    <option value="Afghanistan">Afghanistan</option>
+                    <option value="Australia">Australia</option>
+                    <option value="Canada">Canada</option>
+                    <option value="China">China</option>
+                    <option value="Germany">Germany</option>
+                    <option value="India">India</option>
+
+                    <option value="Nepal">Nepal</option>
+
+                    <option value="Sri Lanka">Sri Lanka</option>
+
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="Ukraine">Ukraine</option>
+                    <option value="United States">USA</option>
+                  </Select>
+                    
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>Contact Number</FormLabel>
+                  <Input
+                    type="number"
+                    backgroundColor={"#c1d6f3"}
+                    required
+                    value={detail.contact}
+                    name="contact"
+                    placeholder="Contact Number"
+                    onChange={handleChange}
+                  />
+                </FormControl>
+
+                <FormControl>
+                  <FormLabel>State</FormLabel>
+                  <Input
+                    type="text"
+                    required
+                    backgroundColor={"#c1d6f3"}
+                    value={detail.state}
+                    name="state"
+                    placeholder="State"
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <br />
+                <Button
+                  bg={"#0f346c"}
+                  colorScheme={"white"}
+                  variant={"solid"}
+                  type="submit"
+                  value={"submit"}
+                >
+                  Submit Personal Info
+                </Button>
+                <Button
+                  onClick={handleCancel}
+                  m={5}
+                  bg={"red.500"}
+                  colorScheme={"white"}
+                  variant={"solid"}
+                >
+                  Cancel Your Meal
+                </Button>
+              </form>
+              {/* <Input type="submit" value="Submit" /> */}
             </Box>
-         )}
-      </Box>
-   );
+            <Box>
+              <br />
+              <br />
+              <br />
+              <Image
+                borderBottomRightRadius={"50px"}
+                src={
+                  "https://img.freepik.com/premium-vector/online-registration-illustration-design-concept-websites-landing-pages-other_108061-938.jpg?size=626&ext=jpg&ga=GA1.2.1056998353.1659457435&semt=ais"
+                }
+              />
+            </Box>
+          </SimpleGrid>
+        </Box>
+      )}
+      <br />
+      <br />
+      <br />
+    </Box>
+  );
 };
 
 export default PersonalDetails;
